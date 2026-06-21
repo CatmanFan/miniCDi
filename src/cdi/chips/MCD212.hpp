4367873f65c389722dbacc480be854c8b5bf5511
CatmanFan <oussama-studying@hotmail.com>
2026-06-21 03:53:47 +0100

    Fix backdrop on VDSC [MCD212]

--- src/cdi/chips/MCD212.hpp
+++ src/cdi/chips/MCD212.hpp
@@ -441,15 +441,19 @@ class MCD212
 										| 0xFF;
 				else {
 					// Transparent, draw backdrop.
-					switch (reg.BackdropColor & 0x07) {
-						default: output[outputPixel] = 0x101010ff; break;
-						case 0x01: output[outputPixel] = reg.BackdropColor & 0x08 ? 0x1010FFff : 0x101090ff; break;
-						case 0x02: output[outputPixel] = reg.BackdropColor & 0x08 ? 0x10FF10ff : 0x109010ff; break;
-						case 0x03: output[outputPixel] = reg.BackdropColor & 0x08 ? 0x10FFFFff : 0x109090ff; break;
-						case 0x04: output[outputPixel] = reg.BackdropColor & 0x08 ? 0xFF1010ff : 0x901010ff; break;
-						case 0x05: output[outputPixel] = reg.BackdropColor & 0x08 ? 0xFF10FFff : 0x901090ff; break;
-						case 0x06: output[outputPixel] = reg.BackdropColor & 0x08 ? 0xFFFF10ff : 0x909010ff; break;
-						case 0x07: output[outputPixel] = reg.BackdropColor & 0x08 ? 0xFFFFFFff : 0x909090ff; break;
+					if (reg.Icm[0] == Off && reg.Icm[1] == Off)
+						output[outputPixel] = 0x101010ff;
+					else {
+						switch (reg.BackdropColor & 0x07) {
+							default: output[outputPixel] = 0x101010ff; break;
+							case 0x01: output[outputPixel] = reg.BackdropColor & 0x08 ? 0x1010FFff : 0x101090ff; break;
+							case 0x02: output[outputPixel] = reg.BackdropColor & 0x08 ? 0x10FF10ff : 0x109010ff; break;
+							case 0x03: output[outputPixel] = reg.BackdropColor & 0x08 ? 0x10FFFFff : 0x109090ff; break;
+							case 0x04: output[outputPixel] = reg.BackdropColor & 0x08 ? 0xFF1010ff : 0x901010ff; break;
+							case 0x05: output[outputPixel] = reg.BackdropColor & 0x08 ? 0xFF10FFff : 0x901090ff; break;
+							case 0x06: output[outputPixel] = reg.BackdropColor & 0x08 ? 0xFFFF10ff : 0x909010ff; break;
+							case 0x07: output[outputPixel] = reg.BackdropColor & 0x08 ? 0xFFFFFFff : 0x909090ff; break;
+						}
 					}
 				}
 

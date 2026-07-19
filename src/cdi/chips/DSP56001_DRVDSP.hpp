b3cf6cfa1868ffc1ede82bc2a4baa0b4a1aa8fbe
CatmanFan <oussama-studying@hotmail.com>
2026-07-19 05:23:43 +0100

    [DSP] Actually make it so that it boots

--- src/cdi/chips/DSP56001_DRVDSP.hpp
+++ src/cdi/chips/DSP56001_DRVDSP.hpp
@@ -1,31 +1,58 @@
 #ifndef MINICDI_DSP56001_DRVDSP
 #define MINICDI_DSP56001_DRVDSP
 
-// @004346AA(dspdriv) DSP ICR 88
-// @004346D2(dspdriv) DSP ICR 80
-// @00434744(dspdriv) DSP CMD 93
-// @00434744(dspdriv) DSP STAT 0000 0000
-// @004347B2(dspdriv) DSP CMD 95[001301]
-// @004347B2(dspdriv) DSP AUDIO2 1301
-// @00434830(dspdriv) SLAVE PORT D <= $FA
-// @00434848(dspdriv) SLAVE PORT D <= $B0000000
-// SLAVE PORT D => $B0000000
+/**
+	@00434990(dspdriv) DSP <= 0C0000
+	@00434990(dspdriv) WR.B 0030000F <= 00 [S] .LSB
+	@00434984(dspdriv) WR.B 0030000B <= 0C [S] .HSB
+	@0043498A(dspdriv) WR.B 0030000D <= 00 [S] .MSB
+	@00434990(dspdriv) DSP <= 0C0000
+	@00434990(dspdriv) WR.B 0030000F <= 00 [S] .LSB
+	@004346D2(dspdriv) WR.B 00300001 <= 80 [S] .ICR
+	@00434736(dspdriv) RD.B 00300003 => 00 [S] .CVR
+	@00434744(dspdriv) DSP ==> 000000 000000
+	@00434744(dspdriv) WR.B 00300003 <= 93 [S] .CVR
+	@0043474A(dspdriv) RD.B 00300005 => 03 [S] .ISR
+	@00434754(dspdriv) RD.B 0030000B => 00 [S] .HSB
+	@0043475A(dspdriv) RD.B 0030000D => 00 [S] .MSB
+	@00434760(dspdriv) DSP => 000000
+	@00434760(dspdriv) RD.B 0030000F => 00 [S] .LSB
+	@00434764(dspdriv) RD.B 00300005 => 03 [S] .ISR
+	@00434770(dspdriv) RD.B 0030000D => 00 [S] .MSB
+	@00434776(dspdriv) DSP => 000000
+	@00434776(dspdriv) RD.B 0030000F => 00 [S] .LSB
+	@00434786(dspdriv) RD.B 00300005 => 02 [S] .ISR
+	@00434794(dspdriv) WR.B 0030000B <= 00 [S] .HSB
+	@0043479E(dspdriv) WR.B 0030000D <= 13 [S] .MSB
+	@004347A2(dspdriv) DSP <= 001301
+	@004347A2(dspdriv) WR.B 0030000F <= 01 [S] .LSB
+	@004347A6(dspdriv) RD.B 00300003 => 13 [S] .CVR
+	@004347B2(dspdriv) WR.B 00300003 <= 95 [S] .CVR
+**/
 
 class DRVDSP
 {
 	SCC68070* _68070;
 	uint8_t* memory;
 
-	uint8_t ICR;
-	uint8_t CVR;
-	uint8_t ISR;
-	uint8_t IVR;
-	uint32_t RTX; // byte receive (RX) when read, byte transmit (TX) when written
+	uint8_t ICR = 0;
+	uint8_t CVR = 0;
+	uint8_t ISR = 0x02;
+	uint8_t IVR = 0;
+	uint32_t RTX = 0; // byte receive (RX) when read, byte transmit (TX) when written
 
-	inline void check_isr()
+	std::deque<uint8_t> RX; // to be read by CPU
+
+	inline void update_irq()
 	{
-		if (IVR > 0)
-			_68070->interrupt(SCC68070::IPL_IN4N, ((ISR & 0b00000001) && (ICR & 0b00000001)) || ((ISR & 0b00000010) && (ICR & 0b00000010)));
+		if (((ISR & 0x01) && (ICR & 0x01))/* || ((ISR & 0x02) && (ICR & 0x02))*/)
+		{
+			_68070->interrupt(SCC68070::IPL_IN4N, true);
+		}
+		else
+		{
+			_68070->interrupt(SCC68070::IPL_IN4N, false);
+		}
 	}
 
 	CDiDisc *disc;
@@ -56,16 +83,25 @@ public:
 				// MiniCDI::Log("[DSP] ISR => %02X", ISR);
 				return ISR;
 			case 0x300007:
-				MiniCDI::Log("[DSP] IVR => %02X", IVR);
+				// MiniCDI::Log("[DSP] IVR => %02X", IVR);
 				return IVR;
 			case 0x30000B:
 				return RTX >> 16 & 0xFF;
 			case 0x30000D:
 				return RTX >> 8 & 0xFF;
 			case 0x30000F:
-				MiniCDI::Log("[DSP] send %06X to host", RTX);
-				ISR &= ~0x01; // deassert RX full
-				check_isr();
+				MiniCDI::Log("[DSP] RX => %06X", RTX);
+				if (RX.size() % 3 == 0) {
+					uint8_t value = RTX & 0xFF;
+					RTX = (RX[0] << 16) | (RX[1] << 8) | RX[2];
+					RX.pop_front();
+					RX.pop_front();
+					RX.pop_front();
+					return value;
+				}
+
+				ISR &= ~0x01; // RXDF = receiver empty
+				update_irq();
 				return RTX & 0xFF;
 		}
 	}
@@ -82,9 +118,8 @@ public:
 				break;
 			case 0x300003: CVR = value;
 				if (CVR & 0x80) {
-					CVR &= 0x1F;
 					// Commands usually write A NUMBER OF argument values to RTX and THEN read result values from the same register. Prefix parameter is optional.
-					switch (CVR) {
+					switch (CVR & 0x1F) {
 						default:
 							break;
 						case 0x00:
@@ -102,10 +137,18 @@ public:
 						case 0x12:
 							MiniCDI::Log("[DSP] read audio status (0x%02X : %06X)", value, RTX);
 							break;
+
 						case 0x13:
 							MiniCDI::Log("[DSP] read status (0x%02X : %06X)", value, RTX);
 							// this command writes and reads two pairs of 24-bit values ? (0C0000 0C0000)
+							RX = {0x00,0x00,0x00, 0x00,0x00,0x00};
+							RTX = (RX[0] << 16) | (RX[1] << 8) | RX[2];
+
+							CVR &= 0x1F;
+							ISR |= 0x01; // RXDF = receiver full
+							update_irq();
 							break;
+
 						case 0x14:
 							MiniCDI::Log("[DSP] audio-related? 1 (0x%02X : %06X)", value, RTX);
 							break;
@@ -114,6 +157,7 @@ public:
 							break;
 						case 0x16:
 							MiniCDI::Log("[DSP] start sector (0x%02X : %06X)", value, RTX);
+							assert(0); // Not implemented
 							break;
 						case 0x17:
 							MiniCDI::Log("[DSP] start DMA read (0x%02X : %06X)", value, RTX);
@@ -138,12 +182,13 @@ public:
 							break;
 						case 0x1F:
 							MiniCDI::Log("[DSP] set volume (0x%02X : %06X)", value, RTX);
+							CVR &= 0x1F;
 							break;
 					}
 				}
 				break;
 			case 0x300005: ISR = value;
-				check_isr();
+				update_irq();
 				MiniCDI::Log("[DSP] ISR <= %02X", value);
 				break;
 			case 0x300007: IVR = value;
@@ -155,9 +200,10 @@ public:
 			case 0x30000D: RTX &= 0x00FF00FF; RTX |= value << 8;
 				break;
 			case 0x30000F: RTX &= 0x00FFFF00; RTX |= value;
-				ISR &= ~0x02; // deassert TX empty
-				check_isr();
-				// MiniCDI::Log("[DSP] received %06X from host", value);
+				// ISR &= ~0x02; // deassert TX empty
+				// update_irq();
+				// no need to send anything to the vector.
+				MiniCDI::Log("[DSP] TX <= %06X", RTX);
 				break;
 		}
 	}

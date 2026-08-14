92c1ff07ebabdc6c8459be52daa5b2813ec589c1
CatmanFan <oussama-studying@hotmail.com>
2026-08-15 00:24:52 +0100

    [wxWidgets] Basic UI frontend is now complete

--- src/frontends/wx/mainFrame.cpp
+++ src/frontends/wx/mainFrame.cpp
@@ -7,7 +7,13 @@
 #include <SDL2/SDL.h>
 #include <filesystem>
 
+#include "app_icon.xpm"
+
 using namespace miniCDi;
+
+static PhilipsCDI *cdi = NULL;
+#define CDI_SCREEN_WIDTH 768
+#define CDI_SCREEN_HEIGHT 280
 
 /*******************************************************************************
 // BasicDrawPane
@@ -17,10 +23,6 @@ BEGIN_EVENT_TABLE(BasicDrawPane, wxPanel)
 
 	// some useful events
 	/*
-	 EVT_MOTION(BasicDrawPane::mouseMoved)
-	 EVT_LEFT_DOWN(BasicDrawPane::mouseDown)
-	 EVT_LEFT_UP(BasicDrawPane::mouseReleased)
-	 EVT_RIGHT_DOWN(BasicDrawPane::rightClick)
 	 EVT_LEAVE_WINDOW(BasicDrawPane::mouseLeftWindow)
 	 EVT_KEY_DOWN(BasicDrawPane::keyPressed)
 	 EVT_KEY_UP(BasicDrawPane::keyReleased)
@@ -29,18 +31,40 @@ BEGIN_EVENT_TABLE(BasicDrawPane, wxPanel)
 
 	// catch paint events
 	EVT_PAINT(BasicDrawPane::paintEvent)
+	EVT_MOTION(BasicDrawPane::mouseControl)
+	EVT_LEFT_DOWN(BasicDrawPane::mouseControl)
+	EVT_LEFT_UP(BasicDrawPane::mouseControl)
+	EVT_RIGHT_DOWN(BasicDrawPane::mouseControl)
+	EVT_RIGHT_UP(BasicDrawPane::mouseControl)
 
 END_EVENT_TABLE()
 
-void BasicDrawPane::paintEvent(wxPaintEvent & evt)
+void BasicDrawPane::paintEvent(wxPaintEvent& WXUNUSED(event))
 {
 	wxAutoBufferedPaintDC dc(this);
 	if (image.IsOk())
 	{
 		int width, height;
 		wxWindow::GetClientSize(&width, &height);
-		wxBitmap bmp(image.Scale(width, height, wxIMAGE_QUALITY_NORMAL));
+		wxBitmap bmp(image.Scale(width, height, wxIMAGE_QUALITY_BOX_AVERAGE));
 		dc.DrawBitmap(bmp, 0, 0);
+	}
+}
+
+void BasicDrawPane::mouseControl(wxMouseEvent& WXUNUSED(event))
+{
+	if (cdi != NULL)
+	{
+		const wxMouseState state = wxGetMouseState();
+		cdi->pd.set_button(PointingDevice::Button1, state.LeftIsDown());
+		cdi->pd.set_button(PointingDevice::Button2, state.RightIsDown());
+
+		wxPoint panel_coords = this->GetScreenPosition();
+		wxPoint mouse_coords = state.GetPosition();
+		int x = mouse_coords.x - panel_coords.x, y = mouse_coords.y - panel_coords.y;
+		int width, height;
+		wxWindow::GetClientSize(&width, &height);
+		cdi->pd.set_coord(x, y, width, height);
 	}
 }
 
@@ -48,13 +72,10 @@ void BasicDrawPane::paintEvent(wxPaintEvent & evt)
 // SDLPanel Class
 *******************************************************************************/
 
-static PhilipsCDI *cdi = NULL;
-#define CDI_SCREEN_WIDTH 768
-#define CDI_SCREEN_HEIGHT 280
-
 BEGIN_EVENT_TABLE(mainFrame, wxFrame)
 	EVT_IDLE(mainFrame::e_idle)
 	EVT_MENU(wxID_OPEN_ROM, mainFrame::e_openSystemROM)
+	EVT_MENU(wxID_OPEN_DISC, mainFrame::e_openDisc)
 	EVT_MENU(wxID_RESET_MACHINE, mainFrame::e_reset)
 	EVT_MENU(wxID_EXIT, mainFrame::e_exit)
 	EVT_MENU(wxID_ABOUT, mainFrame::e_about)
@@ -63,30 +84,37 @@ END_EVENT_TABLE()
 mainFrame::mainFrame(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style) : wxFrame(parent, id, title, pos, size, style)
 {
 	this->SetSizeHints(wxDefaultSize, wxDefaultSize);
-	this->DragAcceptFiles(true);
+	// this->DragAcceptFiles(true);
 
 	// Create menus
-	wxMenu *m_menuFile = new wxMenu;
-	m_menuFile->Append(wxID_OPEN_ROM, wxString(_("Create Mono-&1 machine...")) + wxT('\t') + wxT("Ctrl+O"));
-	m_menuFile->AppendSeparator();
-	m_menuFile->Append(wxID_RESET_MACHINE, wxString(_("&Reset")) + wxT('\t') + wxT("F1"));
-	m_menuFile->AppendSeparator();
-	m_menuFile->Append(wxID_EXIT, wxString(_("E&xit")) + wxT('\t') + wxT("Alt+F4"));
+	wxMenu *menuFile = new wxMenu;
+	menuOpenDisc = new wxMenuItem(menuFile, wxID_OPEN_DISC, wxString(_("Open &disc...")) + wxT('\t') + wxT("Ctrl+O"), wxEmptyString, wxITEM_NORMAL);
+	menuOpenDisc->Enable(false);
 
-	wxMenu *m_menuHelp = new wxMenu;
-	m_menuHelp->Append(wxID_ABOUT, wxString(_("&About")));
+	menuFile->Append(wxID_OPEN_ROM, wxString(_("Create &CD-i machine...")) + wxT('\t') + wxT("Ctrl+C"));
+	menuFile->Append(menuOpenDisc);
+	menuFile->AppendSeparator();
+	menuFile->Append(wxID_RESET_MACHINE, wxString(_("&Reset")) + wxT('\t') + wxT("F1"));
+	menuFile->AppendSeparator();
+	menuFile->Append(wxID_EXIT, wxString(_("E&xit")) + wxT('\t') + wxT("Alt+F4"));
+
+	wxMenu *menuHelp = new wxMenu;
+	menuHelp->Append(wxID_ABOUT, wxString(_("&About")));
 
 	wxMenuBar *menuBar = new wxMenuBar;
-	menuBar->Append(m_menuFile, _("File"));
-	menuBar->Append(m_menuHelp, _("Help"));
+	menuBar->Append(menuFile, _("File"));
+	menuBar->Append(menuHelp, _("Help"));
 	this->SetMenuBar(menuBar);
 
 	// Create panel
 	mainPanel = new BasicDrawPane(this);
 	mainPanel->SetBackgroundColour(wxColour(128, 128, 128));
 
-	this->SetClientSize(768, 560);
+	this->SetClientSize(384, 280);
 	this->Centre(wxBOTH);
+
+	wxIcon icon(app_icon_xpm);
+	SetIcon(icon);
 }
 
 mainFrame::~mainFrame()
@@ -116,16 +144,11 @@ void mainFrame::e_about(wxCommandEvent& WXUNUSED(event))
 }
 
 void mainFrame::e_openSystemROM(wxCommandEvent& WXUNUSED(event))
-{ 
-	if (cdi != NULL)
-	{
-		wxMessageBox(_("A CD-i machine has already been created."), _("Error"), wxOK | wxICON_ERROR);
-		return;
-	}
-
+{
 	wxFileDialog openFileDialog(this, _("Open system ROM"), "", "", "ROM (*.rom)|*.rom", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
 	if (openFileDialog.ShowModal() == wxID_OK)
 	{
+		if (cdi != NULL) { delete cdi; cdi = NULL; }
 		cdi = new PhilipsCDI;
 		const std::filesystem::path rom = std::string(openFileDialog.GetPath().mb_str());
 		enum CDi::BoardType board = rom.stem().compare("cdi490a") == 0 ? CDi::MonoIV
@@ -133,7 +156,16 @@ void mainFrame::e_openSystemROM(wxCommandEvent& WXUNUSED(event))
 								  : CDi::MonoI;
 		cdi->init(rom.string(), board);
 		mainPanel->image = wxImage(CDI_SCREEN_WIDTH, CDI_SCREEN_HEIGHT);
+		if (menuOpenDisc) menuOpenDisc->Enable(true);
 	}
+}
+
+void mainFrame::e_openDisc(wxCommandEvent& WXUNUSED(event))
+{
+	wxFileDialog openFileDialog(this, _("Open disc"), "", "", "Binary (*.bin)|*.bin", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
+
+	if (openFileDialog.ShowModal() == wxID_OK && cdi != NULL)
+		cdi->swap_disc(openFileDialog.GetPath());
 }
 
 void mainFrame::e_idle(wxIdleEvent& WXUNUSED(event))
--- src/frontends/wx/mainFrame.hpp
+++ src/frontends/wx/mainFrame.hpp
@@ -9,22 +9,8 @@ namespace miniCDi
 		BasicDrawPane(wxFrame* parent) : wxPanel(parent) { SetBackgroundStyle(wxBG_STYLE_PAINT); }
 		wxImage image;
 		
-		void paintEvent(wxPaintEvent & evt);
-		void paintNow();
-		
-		void render(wxDC& dc);
-		
-		// some useful events
-		/*
-		 void mouseMoved(wxMouseEvent& event);
-		 void mouseDown(wxMouseEvent& event);
-		 void mouseWheelMoved(wxMouseEvent& event);
-		 void mouseReleased(wxMouseEvent& event);
-		 void rightClick(wxMouseEvent& event);
-		 void mouseLeftWindow(wxMouseEvent& event);
-		 void keyPressed(wxKeyEvent& event);
-		 void keyReleased(wxKeyEvent& event);
-		 */
+		void paintEvent(wxPaintEvent &event);
+		void mouseControl(wxMouseEvent &event);
 		
 		DECLARE_EVENT_TABLE()
 	};
@@ -33,24 +19,25 @@ namespace miniCDi
 	{
 		DECLARE_EVENT_TABLE()
 		void e_openSystemROM(wxCommandEvent &event);
+		void e_openDisc(wxCommandEvent &event);
 		void e_about(wxCommandEvent &event);
 		void e_reset(wxCommandEvent &event);
 		void e_exit(wxCommandEvent &event);
 		void e_idle(wxIdleEvent &event);
-		// void e_fbPaint(wxPaintEvent &event);
 
-		wxImage m_screen;
+		wxMenuItem *menuOpenDisc;
 		BasicDrawPane* mainPanel;
 
 		enum
 		{
 			wxID_GAME_PANEL = 6000,
 			wxID_OPEN_ROM = 6001,
-			wxID_RESET_MACHINE = 6002,
+			wxID_OPEN_DISC = 6002,
+			wxID_RESET_MACHINE = 6003,
 		};
 
 		public:
-			mainFrame(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("miniCDi"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(768,560), long style = wxCLOSE_BOX|wxDEFAULT_FRAME_STYLE|wxMAXIMIZE_BOX|wxMINIMIZE_BOX|wxRESIZE_BORDER|wxSYSTEM_MENU|wxTAB_TRAVERSAL);
+			mainFrame(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("miniCDi"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(-1,-1), long style = wxCLOSE_BOX|wxDEFAULT_FRAME_STYLE|wxMAXIMIZE_BOX|wxMINIMIZE_BOX|wxRESIZE_BORDER|wxSYSTEM_MENU|wxTAB_TRAVERSAL);
 			~mainFrame();
 	};
 }
